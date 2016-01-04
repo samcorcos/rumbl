4877f04192f73796e262d611dcc16b77c6a1d54d
Sam Corcos <sam@corcos.io>
2016-01-03 18:12:53 -0600

    adding watch

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,6 +12,12 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "deps/phoenix_html/web/static/js/phoenix_html"
+import Player from './player'
+let video = document.getElementById("video");
+
+if (video) {
+  Player.init(video.id, video.getAttribute("data-player-id"))
+}
 
 // Import local files
 //
